df1698d5af930998b4ff471c0219d62c94e5b0b8
Soul147 <45136555+Soul147@users.noreply.github.com>
2019-05-14 22:34:28 -0700

    Add files via upload

--- savefixer/savefixer.js
+++ savefixer/savefixer.js
@@ -4,6 +4,13 @@ function fixSave() {
   var fixed = save.replace(/NaN/gi, "10")
   var stillToDo = JSON.parse(fixed)
   for (var i=0; i<stillToDo.autobuyers.length; i++) stillToDo.autobuyers[i].isOn = false
+  stillToDo.mods = stillToDo.mods || {}
+  
+  document.getElementById("devOut").value = JSON.stringify(stillToDo)
   
   document.getElementById("fixed").value = btoa(JSON.stringify(stillToDo))
 }
+
+addEventListener("keydown", function(e) {
+  if(e.keyCode == 27) document.getElementById("devOutSpan").style.display = ""
+})
